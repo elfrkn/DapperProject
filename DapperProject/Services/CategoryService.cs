9d59fd55b2c2a89d1f5563199944f6eb322713b6
Elif ÇALIŞ <pcengelf@hotmail.com>
2024-06-25 14:48:13 +0200

    Add project files.

---
+++ DapperProject/Services/CategoryService.cs
@@ -0,0 +1,32 @@
+﻿using DapperProject.Dtos.CategoryDtos;
+
+namespace DapperProject.Services
+{
+    public class CategoryService : ICategoryService
+    {
+        public Task CreateCategoryAsync(CreateCategoryDto createCategoryDto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteCategoryAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<ResultCategoryDto>> GetAllCategoryAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<GetByIdCategoryDto> GetCategoryAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateCategoryAsync(UpdateCategoryDto updateCategoryDto)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ DapperProject/Services/ICategoryService.cs
@@ -0,0 +1,14 @@
+﻿using DapperProject.Dtos.CategoryDtos;
+
+namespace DapperProject.Services
+{
+    public interface ICategoryService
+    {
+        Task<List<ResultCategoryDto>> GetAllCategoryAsync();
+        Task CreateCategoryAsync(CreateCategoryDto createCategoryDto);
+        Task DeleteCategoryAsync(int id);
+        Task UpdateCategoryAsync(UpdateCategoryDto updateCategoryDto);
+        Task<GetByIdCategoryDto> GetCategoryAsync(int id);
+        
+    }
+}
